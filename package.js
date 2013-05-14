06ad527e327c34fb397990fdb1c18e5f7ae893a7
antda <anton.danielsson@gmail.com>
2013-05-14 11:31:24 +0200

    Added animation for when new objects are added

--- package.js
+++ package.js
@@ -3,9 +3,11 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+  api.use(['templating'], 'client');
   api.add_files([
     'lib/metromet.js',
     'lib/metromet.css',
+    'lib/metromet.html',
   ], 'client'
   );
 });
